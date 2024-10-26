d2ebe34325e6543fb1344d334d73dd191668c1d7
thommccarthy <mccarttj@gmail.com>
2024-10-26 10:05:03 -0400

    remove vid carousel add tick toxins vid

--- src/components/videoCarousel.js
+++ src/components/videoCarousel.js
@@ -7,59 +7,18 @@ const VideoCarousel = () => {
   return (
     <div id="video" className={videoCarouselStyles.carouselWrapper}>
       <h2>VIDEO</h2>
-      <Carousel
-        infiniteLoop={true}
-        autoPlay={false}
-        showThumbs={false}
-        swipeable={true}
-        showStatus={false}
-      >
-        <div className={videoCarouselStyles.videoWrapper}>
-          <iframe
-            width="1920"
-            height="1080"
-            src="https://www.youtube.com/embed/NHuJ10bLWNA"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-          ></iframe>
-        </div>
-        <div className={videoCarouselStyles.videoWrapper}>
-          <iframe
-            width="1920"
-            height="1080"
-            src="https://www.youtube.com/embed/LLwEQcFrkrY"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-          ></iframe>
-        </div>
 
-        <div className={videoCarouselStyles.videoWrapper}>
-          <iframe
-            width="1920"
-            height="1080"
-            src="https://www.youtube.com/embed/HJm1QFSazLA"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-          ></iframe>
-        </div>
-        <div className={videoCarouselStyles.videoWrapper}>
-          <iframe
-            width="1920"
-            height="1080"
-            src="https://www.youtube.com/embed/dlRgoSITkBI"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-          ></iframe>
-        </div>
-      </Carousel>
+      <div className={videoCarouselStyles.videoWrapper}>
+        <iframe
+          width="1920"
+          height="1080"
+          src="https://www.youtube.com/embed/1rzW3XIQTQk"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
     </div>
   )
 }
